939570b6201ee15dc38cd5aaa430645aff43b7f1
Nikoloz Shervashidze <nikoshervashidze@gmail.com>
2023-01-29 20:24:45 +0400

    add tab change functionality

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 const nav = document.querySelector(".nav");
+const navOuter = document.querySelector("#nav-outer");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const navLinks = document.querySelectorAll(".nav__link");
 const openModal = document.querySelector(".about__btn");
 const modal = document.querySelector(".instructions");
+const modalClose = document.querySelector(".instructions__buttons__close");
 
 openModal.addEventListener("click", () => {
 	modal.showModal();
+	document.body.classList.toggle("stop-scrolling");
+});
+
+modalClose.addEventListener("click", () => {
+	modal.close();
+	document.body.classList.toggle("stop-scrolling");
+});
+
+modal.addEventListener("click", (e) => {
+	console.log(e.target);
+	if (e.target === modal) {
+		modal.close();
+		document.body.classList.toggle("stop-scrolling");
+	}
 });
 
 navToggle.addEventListener("click", () => {
@@ -36,3 +52,35 @@ new ScrollBooster({
 	direction: "horizontal", // allow only horizontal scrolling
 	textSelection: "true",
 });
+
+const tabs = document.querySelectorAll(".tabcontent");
+const dots = document.querySelectorAll(".dots");
+const nextButton = document.querySelector(".instructions__buttons__next");
+
+let currentTab = 0;
+
+dots.forEach((dot, index) => {
+	dot.addEventListener("click", () => {
+		currentTab = index;
+		updateTabs();
+	});
+});
+
+nextButton.addEventListener("click", () => {
+	currentTab = (currentTab + 1) % tabs.length;
+	updateTabs();
+});
+
+function updateTabs() {
+	tabs.forEach((tab, index) => {
+		if (index === currentTab) {
+			tab.style.display = "block";
+			dots[index].classList.add("active");
+		} else {
+			tab.style.display = "none";
+			dots[index].classList.remove("active");
+		}
+	});
+}
+
+updateTabs();
